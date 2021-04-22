f4f6fc32b5509234da7eb9ec04befa1347ffd0fc
Raveena Kholia <raveenakholia786@gmail.com>
2021-04-22 23:24:10 +0530

    Added a task button in all task card

--- src/Components/Containers/TaskContainer.jsx
+++ src/Components/Containers/TaskContainer.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './TaskContainer.scss'
 
 export default function TaskContainer(props) {
+   let toBeVisible= false;
     return (
         <div className="taskDiv">
             <h2>{props.taskName}</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, sapiente! Quam debitis nam doloremque quia consectetur, aut veritatis quos nulla dignissimos cum dolorem. Fuga, provident. Esse at fugit doloremque numquam.</p>  
+        { props.taskName=="All Task"?toBeVisible=true:toBeVisible=false}
+        {toBeVisible== true?<><input/><button>Add A Task</button></>:null}
         </div>
     )
 }
