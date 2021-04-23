99ce3f07a9d8ee32743240275aa882a57afd1fc6
Raveena Kholia <raveenakholia786@gmail.com>
2021-04-23 20:58:31 +0530

    ok

--- src/Components/Task/Card.jsx
+++ src/Components/Task/Card.jsx
@@ -1,6 +1,7 @@
 import "./Card.scss";
 export default function Card({ desc, createdOn, headerColor }) {
   return (
+    
     <div className="card" style={{ borderTop: `4px solid ${headerColor}` }}>
       {desc}
       <div className="footer">{createdOn}</div>
